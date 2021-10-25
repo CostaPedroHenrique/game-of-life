23940b3a5ff6de8d055444d9e68302134db25ef4
CostaPedroHenrique <devpedrohenrique@gmail.com>
2021-10-25 14:12:43 -0300

    feat: initialization project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # game-of-life
 Projeto desenvolvido no curso Passaporte DEV Sênior, do Henrique Bastos
+
+python3 -m gameoflife
---
+++ gameoflife/__main__.py
@@ -0,0 +1,10 @@
+import sys
+
+from gameoflife.cli import cli
+from gameoflife.gui import gui
+
+if len(sys.argv) > 1:
+
+    gui()
+else:
+    cli()
